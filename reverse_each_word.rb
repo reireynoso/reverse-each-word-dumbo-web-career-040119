98109dfb9dfa2965d3ed4188443636cf9aa60c42
Reinald Reynoso <reireynoso@gmail.com>
2019-03-11 18:47:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(sentence) 
   string = " "
   example = []
-  array_sentence = sentence.split(" ")
+  array_sent = sentence.split(" ")
+  array_sentence = array_sent.split("")
   array_sentence.collect do |letter|
     example.push(letter) 
   end

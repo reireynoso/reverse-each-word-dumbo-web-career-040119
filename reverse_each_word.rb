0052290b88458471895a5ba5259a84e1761bbb91
Reinald Reynoso <reireynoso@gmail.com>
2019-03-11 18:30:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence) 
-  sentence.split(" ")
-  sentence.each do |letter|
+  array_sentence = sentence.split(" ")
+  array_sentence.each do |letter|
     puts letter
   end
 end
